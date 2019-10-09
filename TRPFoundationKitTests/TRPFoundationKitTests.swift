552b0e18f4e890677d63d6c933aefdfa55ffd436
Evren Yasar <evren@necatievren.com>
2019-10-09 14:03:05 +0300

    Unit tes

---
+++ TRPFoundationKitTests/TRPFoundationKitTests.swift
@@ -0,0 +1,25 @@
+//
+//  TRPFoundationKitTests.swift
+//  TRPFoundationKitTests
+//
+//  Created by Evren Yaşar on 9.10.2019.
+//  Copyright © 2019 Evren Yaşar. All rights reserved.
+//
+
+import XCTest
+
+@testable import TRPFoundationKit
+class TRPFoundationKitTests: XCTestCase {
+
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let demo = "".toLocation()
+        XCTAssertNil(demo
+        )
+    }
+
+    
+
+}
